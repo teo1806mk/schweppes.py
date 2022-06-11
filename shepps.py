aadb3b89800fef2d2b4321ad64f7d8faf90ee656
teo20 <karipidis2001theodor@gmail.com>
2022-06-11 13:05:33 +0300

    main files

--- shepps.py
+++ shepps.py
@@ -1,21 +1,20 @@
 import time
 import smtplib
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import random
-
-p = "0"
+p="0"
 from playsound import playsound
-from datetime import datetime
-
 
 def email():
+
+
     gmail_user = 'karipidis2001theodor@gmail.com'
     gmail_password = 'ddytgvcefbgzjnsi'
 
+
     sent_from = gmail_user
-    to = ['teo2005lol@gmail.com', 'karipidis73@gmail.com', 'mariannavourakii@gmail.com', 'pdimis01@gmail.com']
+    to = ['teo2005lol@gmail.com', 'karipidis73@gmail.com','mariannavourakii@gmail.com','pdimis01@gmail.com']
     subject = 'pare grigora thlefono ton thodori karypidi'
     body = 'PARETILEFONO TON THODORI'
 
@@ -23,6 +22,7 @@ def email():
     From: %s
     To: %s
     Subject: %s
+
     %s
     """ % (sent_from, ", ".join(to), subject, body)
 
@@ -38,79 +38,63 @@ def email():
 
 
 def paraskevas():
-    login = driver.find_element(by=By.ID, value="signInEmailAddress")
+    login = driver.find_element_by_id("signInEmailAddress")
     login.click()
     login.send_keys("hdjfic2001@gmail.com")
-    pascod = driver.find_element(by=By.ID, value="currentPassword")
+    pascod = driver.find_element_by_id("currentPassword")
     pascod.click()
     pascod.send_keys("18062001!Ka")
     time.sleep(2)
-    driver.find_element(by=By.CSS_SELECTOR, value="#ces-form-submit-ces-sign-in-form").click()
+    booton = driver.find_element_by_id("ces-form-submit-ces-sign-in-form").click()
     time.sleep(3)
-    dora = driver.find_element(by=By.CSS_SELECTOR,
-                               value="#global-page-wrapper > div.scroll-content > main > div > div > div > div.split-layout__sidebar > div > div.default-sidebar__body > ul > li:nth-child(4) > a > div.label")
+    dora = driver.find_element_by_css_selector("#global-page-wrapper > div.scroll-content > main > div > div > div > div.split-layout__sidebar > div > div.default-sidebar__body > ul > li:nth-child(4) > a > div.label")
     time.sleep(3)
     dora.click()
 
 
-driver = webdriver.Chrome(executable_path=r"A:\bot sp\chromedriver.exe")
-driver.set_window_size(1024, 800)
+driver = webdriver.Chrome(executable_path=r"C:\Users\teo20\OneDrive\Υπολογιστής\bot sp\chromedriver.exe")
+
 url = (f"https://www.schweppes.gr/login.php")
 driver.get(url)
 
-time.sleep(5)
-driver.find_element(by=By.ID, value="accept-recommended-btn-handler").click()
+time.sleep(6)
+cookes = driver.find_element_by_id("accept-recommended-btn-handler").click()
 paraskevas()
 k = 0
 time.sleep(2)
 pp = 0
 while p == "0":
     print(k)
-    time.sleep(60)
+    time.sleep(2)
     try:
-        # getting gifts
-        print('getting gifts')
-        gift_container = driver.find_element(by=By.ID, value='userListPrize')
-        gifts = gift_container.find_elements(by=By.TAG_NAME, value='li')
 
-        # for each gift
-        for gift in gifts:
-            class_name = gift.get_attribute('class')
-            image_alt = gift.find_element(by=By.TAG_NAME, value='img').get_attribute('alt')
-            if class_name == 'prize-list-item item-active' and image_alt == 'Mi Electric Scooter - Xiaomi':
-                p = "1"
-            # if class_name == 'prize-list-item item-active' and image_alt == 'Eva Fuda - μπρελόκ κίτρινο & μαύρο':
-            #    p = '1'
-
-    except:
+        poso = driver.find_element_by_xpath(f"/html/body/div[1]/div[1]/main/div/div/div/div[2]/div/div[2]/ul/li[11]/div[1]/div/span[2]").clear()
+        print(poso , "poso")
+        p = str(poso.text)
+    except :
         time.sleep(10)
         try:
-            driver.find_element(by=By.CSS_SELECTOR,
-                                value='#global-page-wrapper > div.scroll-content > main > div > div > div > div.split-layout__sidebar > div > div.default-sidebar__body > ul > li:nth-child(4) > a > div.label').click()
+            driver.find_element_by_css_selector("#global-page-wrapper > div.scroll-content > div > div.container > div > div > div > a.btn.btn--md.btn--yellow.jsAgeButtonYes").click()
+            time.sleep(6)
+            driver.get(url)
+            time.sleep(6)
+            paraskevas()
         except:
             try:
-                time.sleep(2)
-                driver.find_element(by=By.CSS_SELECTOR,
-                                    value="#global-page-wrapper > div.scroll-content > div > div.container > div > div > div > a.btn.btn--md.btn--yellow.jsAgeButtonYes").click()
                 time.sleep(6)
                 driver.get(url)
                 time.sleep(6)
                 paraskevas()
             except:
-                try:
-                    time.sleep(6)
-                    driver.get(url)
-                    time.sleep(6)
-                    paraskevas()
-                except:
-                    p = "0"
+                p = "0"
 
+    k += 1
     if p != "0":
-        email()
+        #email()
         playsound("Rick_Astley_-_Never_Gonna_Give_You_Up_Official_Music_Video[ConConverter.com].mp3")
     else:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(k, current_time)
+        print(k, time.time())
         k += 1
         driver.refresh()
+
+
